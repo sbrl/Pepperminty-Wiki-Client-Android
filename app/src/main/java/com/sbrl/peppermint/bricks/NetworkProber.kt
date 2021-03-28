66f9fbadfee78e1895ea179c762b0ac980f4a147
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-03-28 14:04:53 +0100

    Let's have another go.

--- app/src/main/java/com/sbrl/peppermint/bricks/NetworkProber.kt
+++
@@ -1,9 +0,0 @@
-package com.sbrl.peppermint.bricks
-
-import android.content.Context
-import android.net.wifi.WifiManager
-
-fun is_wifi_enabled(context : Context) : Boolean {
-	return (context.applicationContext
-		.getSystemService(Context.WIFI_SERVICE) as WifiManager).isWifiEnabled
-}
