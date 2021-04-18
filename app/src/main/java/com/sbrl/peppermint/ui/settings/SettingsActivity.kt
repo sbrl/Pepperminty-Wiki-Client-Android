bc0bc83b5c9a7607b29ea965256a553ce726a1de
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-04-19 00:29:59 +0100

    Fill out settings, but they don't do anything just yet

--- app/src/main/java/com/sbrl/peppermint/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/sbrl/peppermint/ui/settings/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.sbrl.peppermint.ui.settings
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import com.sbrl.peppermint.R
@@ -17,6 +19,23 @@ class SettingsActivity : AppCompatActivity() {
 				.commit()
 		}
 		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+	}
+	
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		// HACK: We just blindly open the drawer here, because we can't figure out an effective way to tellt he buttons on the action bar (that's the bar at the top of the screen) apart without replacing it with our own toolbar...... grumble
+		
+		// TODO: Save the settings here
+		Log.w("SettingsActivity", "TODO: Save the settings here onOptionsItemSelected")
+		
+		finish()
+		
+		super.onOptionsItemSelected(item)
+		return true // Allow normal processing to continue
+	}
+	
+	override fun onNavigateUp(): Boolean {
+//		finish()
+		return true
 	}
 	
 	class SettingsFragment : PreferenceFragmentCompat() {
