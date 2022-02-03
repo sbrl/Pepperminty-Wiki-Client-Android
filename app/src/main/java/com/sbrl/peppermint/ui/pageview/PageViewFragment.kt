6ccbf03f2d7e6f41f96edbdba95b193aac69d477
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-02-03 02:27:48 +0000

    Bugfix: same bug, different place

--- app/src/main/java/com/sbrl/peppermint/ui/pageview/PageViewFragment.kt
+++ app/src/main/java/com/sbrl/peppermint/ui/pageview/PageViewFragment.kt
@@ -71,11 +71,11 @@ class PageViewFragment : Fragment() {
 		swipeRefresh.isRefreshing = true
 	}
 	private fun uiFinishPageViewRefresh(fromCache: Boolean) {
+		swipeRefresh.isRefreshing = false
 		if(fromCache) {
 			val message = getString(R.string.toast_page_view_refreshed) + " " +
 				getString(R.string.toast_addon_from_cache)
 			
-			swipeRefresh.isRefreshing = false
 			show_toast(context, message)
 		}
 	}
